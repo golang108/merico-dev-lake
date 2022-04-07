fffeb26ad4efca2b0187411d676394b2dfa98514
zhangliang <liang.zhang@merico.dev>
2022-04-07 22:55:06 +0800

    fix: sync archived  with the model defination

--- models/migrationscripts/archived/pipeline.go
+++ models/migrationscripts/archived/pipeline.go
@@ -8,15 +8,17 @@ import (
 )
 
 type Pipeline struct {
-	Name          string `gorm:"index"`
-	BlueprintId   uint64
-	Tasks         datatypes.JSON
-	TotalTasks    int
+	common.Model
+	Name        string `json:"name" gorm:"index"`
+	BlueprintId uint64
+	Tasks       datatypes.JSON
+	TotalTasks  int
+	// Deprecated
 	FinishedTasks int
 	BeganAt       *time.Time
 	FinishedAt    *time.Time `gorm:"index"`
 	Status        string
 	Message       string
 	SpentSeconds  int
-	common.Model
+	Step          int
 }
