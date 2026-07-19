b4b53b8a6da9e528237b6f0d1c638a8bd331c0b5
Dirk <DoDiDev@proton.me>
2026-07-19 05:24:46 +0200

    fix(domain): migrate issue component to text (#8987)
    Signed-off-by: DoDiODev <DoDiDev@proton.me>
    Co-authored-by: Klesh Wong <klesh@qq.com>

--- backend/core/models/migrationscripts/register.go
+++ backend/core/models/migrationscripts/register.go
@@ -145,6 +145,7 @@ func All() []plugin.MigrationScript {
 		new(modifyCicdDeploymentsToText),
 		new(increaseCqIssuesProjectKeyLength),
 		new(addAuthSessions),
+		new(changeIssueComponentToText),
 		new(changeCqIssueCodeBlocksComponentToText),
 		new(addCqProjectMetricsHistory),
 	}
